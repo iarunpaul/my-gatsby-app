16942b7d38aaf914aa40f9acac1b7cd4efd76efa
apaul4ariqt <apaul@ariqt.com>
2025-09-05 00:44:02 +0100

    adds gif for blog part1

--- gatsby-config.js
+++ gatsby-config.js
@@ -145,6 +145,20 @@ module.exports = {
             },
           },
           {
+            resolve: "gatsby-remark-copy-linked-files",
+            options: {
+              // `ignoreFileExtensions` defaults to [`png`, `jpg`, `jpeg`, `bmp`, `tiff`]
+              // as we assume you'll use gatsby-remark-images to handle
+              // images in markdown as it automatically creates responsive
+              // versions of images.
+              //
+              // If you'd like to not use gatsby-remark-images and just copy your
+              // original images to the public directory, set
+              // `ignoreFileExtensions` to an empty array.
+              // ignoreFileExtensions: [],
+            },
+          },
+          {
             resolve: `gatsby-remark-prismjs`,
             options: {
               classPrefix: "language-",
