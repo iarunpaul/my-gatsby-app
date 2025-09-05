ac3191c91eff55e6bb1e8b14a8cc4d1865549165
apaul4ariqt <apaul@ariqt.com>
2025-09-05 03:29:16 +0100

    fix build error

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,6 @@
 import React from "react";
 import useRotateText from "../hooks/useRotateText";
-import Layout from "../components/layout";
+import { Layout } from "../components/Layout"
 import Seo from "../components/seo";
 
 const AboutPage = () => {
